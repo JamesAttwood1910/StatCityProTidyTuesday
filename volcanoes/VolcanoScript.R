2bc8e9449c40d2fe8457a3ace6925610b6703e23
JamesAttwood1910 <attwood1910@gmail.com>
2020-11-18 11:19:47 -0300

    New graphs and tables

--- volcanoes/VolcanoScript.R
+++ volcanoes/VolcanoScript.R
@@ -26,9 +26,9 @@ cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
 # graph data 
 
 ggplot(data = data, aes(x = as.factor(volcano_name), y = n)) + 
-  geom_bar(stat = "identity", fill = "#F0E442") + theme_minimal() +
+  geom_bar(stat = "identity", fill = "#F0E442", color = "black") + theme_minimal() +
   theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
-  ggtitle("Most active volcanoes") + xlab("Volcano") + ylab("Confirmed erruptions")
+  ggtitle(html("The 10 most active volcanoes")) + xlab("Volcano") + ylab("Confirmed erruptions")
 
 
 ggplot(data = data, aes(x = as.factor(volcano_name), y = n)) + 
@@ -46,7 +46,7 @@ volcano %>% filter(volcano_name %in% top10) %>% select(volcano_name, primary_vol
   
   gt() %>%
   tab_header(
-    title = html("<h3>10 most active volcanoes</h3>")
+    title = html("<h3>The 10 most active volcanoes</h3>")
   )  %>%
   cols_label(volcano_name = md("**Volcano**"), 
              primary_volcano_type = md("**Primary volcano type**"),
@@ -64,8 +64,58 @@ volcano %>% filter(volcano_name %in% top10) %>% select(volcano_name, primary_vol
   ) %>% 
   tab_style(
     style = list(
-      cell_fill(color = "gold", alpha = 0.5)
+      cell_fill(color = "gold", alpha = 0.7)
     ), 
     locations = cells_title(groups = c("title"))
 
-)
+) %>% 
+  tab_style(
+    style = list(
+      cell_fill(color = "gold", alpha = 0.7)
+    ), 
+    locations = cells_title(groups = c("subtitle"))
+  )
+
+
+
+
+volcano %>% filter(volcano_name %in% top10) %>% select(volcano_name, primary_volcano_type, last_eruption_year,
+                                                       country, latitude, longitude, population_within_5_km) %>%
+  
+  gt() %>%
+  tab_header(
+    title = html("<h3>Los 10 volcanes más activos</h3>")
+  )  %>%
+  cols_label(volcano_name = md("**Volcán**"), 
+             primary_volcano_type = md("**Primer tipo de volcán**"),
+             last_eruption_year = md("**Última erupción**"),
+             country = md("**País**"),
+             latitude = md("**Latitud**"),
+             longitude = md("**Longitud**"),
+             population_within_5_km = md("**Población en 5 kilómetros**")
+  ) %>% 
+  tab_style(
+    style = list(
+      cell_fill(color = "yellow", alpha = 0.4)
+    ),
+    locations = cells_body(rows = c(1,3,5,7,9))
+  ) %>% 
+  tab_style(
+    style = list(
+      cell_fill(color = "gold", alpha = 0.7)
+    ), 
+    locations = cells_title(groups = c("title"))
+  ) %>% 
+  tab_style(
+    style = list(
+      cell_fill(color = "gold", alpha = 0.7)
+    ), 
+    locations = cells_title(groups = c("subtitle"))
+  )
+
+
+
+
+
+
+
